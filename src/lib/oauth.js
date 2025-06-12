deef4229f9f6dec9b10fb3d48d88895708ec541f
SHYAM SUNDAR <166678415+Shyam0709@users.noreply.github.com>
2025-06-12 11:48:50 +0530

    Update oauth.js

--- src/lib/oauth.js
+++ src/lib/oauth.js
@@ -1,23 +1,32 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import User from './userModel.js';
-import dotenv from 'dotenv';
-dotenv.config();
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
-}, async (_, __, profile, done) => {
-  let user = await User.findOne({ email: profile.emails[0].value });
-  if (!user) {
-    user = await User.create({
-      email: profile.emails[0].value,
-      provider: 'google'
-    });
+
+export function setupGoogleOAuth({ clientID, clientSecret, callbackURL }) {
+  if (!clientID || !clientSecret || !callbackURL) {
+    throw new Error('Missing required OAuth credentials');
   }
-  return done(null, user);
-}));
+
+  passport.use(new GoogleStrategy({
+    clientID,
+    clientSecret,
+    callbackURL
+  }, async (_, __, profile, done) => {
+    try {
+      let user = await User.findOne({ email: profile.emails[0].value });
+      if (!user) {
+        user = await User.create({
+          email: profile.emails[0].value,
+          provider: 'google'
+        });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err, null);
+    }
+  }));
+}
 
 export function googleAuth() {
   return passport.authenticate('google', { scope: ['profile', 'email'] });
