e16fc3fc2e02dd7662b809af534747307ba90dfb
SHYAM SUNDAR <166678415+Shyam0709@users.noreply.github.com>
2025-06-12 11:49:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import * as auth from './lib/auth.js';
 import * as middleware from './lib/middleware.js';
 import * as refresh from './lib/refresh.js';
-import * as oauth from './lib/oauth.js';
+import {
+  setupGoogleOAuth,
+  googleAuth,
+  googleAuthCallback
+} from './lib/oauth.js';
 import rateLimiter from './lib/rateLimiter.js';
 
 export default {
   ...auth,
   ...middleware,
   ...refresh,
-  ...oauth,
-  rateLimiter
+  setupGoogleOAuth,
+  googleAuth,
+  googleAuthCallback,
+  rateLimiter,
 };
